710de35391c56ccadd47730505ff7e7c8beba289
Shafiq Jetha <shafiq.jetha@outlook.com>
2021-06-18 06:51:16 -0600

    Fix jest issues

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 require('jest-preset-angular/ngcc-jest-processor');
 module.exports = {
   moduleNameMapper: {
-    '@core/(.*)': '<rootDir>/src/app/core/$1',
-    '@shared/(.*)': '<rootDir>/src/app/shared/$1',
+    '@core/(.*)': '<rootDir>/app/core/$1',
+    '@shared/(.*)': '<rootDir>/app/shared/$1',
   },
+  rootDir: 'src',
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  setupFilesAfterEnv: ['../setup-jest.ts'],
 };
